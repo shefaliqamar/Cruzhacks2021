6b32a2132f6641ed70b4d33456a72801282aee10
Shefali Qamar <sqamar@ucsc.edu>
2021-01-15 19:13:01 -0800

    some map stuff

--- SideEffects.py
+++ SideEffects.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import time
 import numpy as np
+import pandas as pd
+from geopy.geocoders import Nominatim
+from geopy.extra.rate_limiter import RateLimiter
+
+geolocator = Nominatim(user_agent="my_application")
+geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
 
 st.title('Covid-19 Vaccine Side Effects')
 
@@ -18,6 +24,23 @@ for i in range(1, 101):
     time.sleep(0.05)
 
 progress_bar.empty()
+
+data = pd.read_csv('se_data.csv')
+
+# ======================================= MAP OF POINTS ===============================
+vaccine_location = data[['Location']]
+vaccine_location['location'] = vaccine_location['Location'].apply(geocode)
+vaccine_location['point'] = vaccine_location['location'].apply(lambda loc: tuple(loc.point) if loc else None)
+
+points = vaccine_location['point'].tolist()
+lat = [lat for (lat, lon, _) in points]
+lon = [lon for (lat, lon, _) in points]
+
+d = {'lat':lat,'lon':lon}
+df = pd.DataFrame(d)
+
+st.map(df)
+# ======================================= END MAP ===============================
 
 # Streamlit widgets automatically run the script from top to bottom. Since
 # this button is not connected to any other logic, it just causes a plain
